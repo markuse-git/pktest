fa8d4d6dd83b1128fd074fe0d669601f9b2fb5ad
Markus <markus.eichelhardt@web.de>
2023-02-23 20:51:30 +0100

    erste fertige Version

--- functions.php
+++ functions.php
@@ -1,13 +1,20 @@
 <?php
 
-// require __DIR__ . '/includes/veranstaltungen.php';
-
 // Enqueue Child CSS
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_uri() );
 }
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles', 11 );
+
+// Enqueue CSS für das Login (selbe CSS s.o.)
+function ourLoginCSS(){
+    wp_enqueue_style('pk_login_style',get_stylesheet_uri());
+}
+
+add_action('login_enqueue_scripts','ourLoginCSS');
+
+// -------------------------------------------------------------------
 
 // pageBanner
 function pageBanner($args = NULL){ 
@@ -32,50 +39,35 @@ function pageBanner($args = NULL){
     echo $args['subtitle'];
 }
 
-// Enable Featured Images
-function pk_features(){
-    add_theme_support('post-thumbnails');
-    add_image_size('landscape', 400, 260, true );
-    add_image_size('portrait', 480, 650, true );
-    add_image_size('pageBanner', 1500, 350, true );
-}
+// -----------------------------------------------------------------
 
-add_action('after_setup_theme', 'pk_features');
+// Enable Featured Images
+// function pk_features(){
+//     add_theme_support('post-thumbnails');
+//     add_image_size('landscape', 400, 260, true );
+//     add_image_size('portrait', 480, 650, true );
+//     add_image_size('pageBanner', 1500, 350, true );
+// }
+
+// add_action('after_setup_theme', 'pk_features');
+
+// --------------------------------------------------------
 
 // Register Custom Post Types 
-
 function pk_post_types(){
-    // Post Type Veranstaltung
-    register_post_type( 'veranstaltung', array(
-        'public' => true,
-        'has_archive' => true,
-        'show_in_rest' => true,
-        'supports' => array('title', 'editor','excerpt', 'comments', 'thumbnail'), # Maskenfelder 
-        'capability_type' => 'veranstaltung', # Un Rechte gesondert zu steuern
-        'map_meta_cap' => true,
-        'rewrite' => array(
-            'slug' => 'veranstaltungen'  
-        ),
-        'menu_icon' => 'dashicons-calendar',
-        'labels' => array(
-            'name' => 'Veranstaltungen',
-            'add_new_item' => 'Neue Veranstaltung',
-            'edit_item' => 'Veranstaltung bearbeiten',
-            'all_items' => 'Alle Veranstaltungen',
-            'singular_name' => 'Veranstaltung'
-        )
-    ) );
 
     // Post Type class
     register_post_type( 'class', array(
         'public' => true,
         'has_archive' => true,
         'show_in_rest' => true,
+        'capability_type' => 'class', # zur Rechtesteuerung in Members
+        'map_meta_cap' => true,
         'supports' => array('title', 'editor','excerpt', 'thumbnail'),  
         'rewrite' => array(
             'slug' => 'classes'  
         ),
-        'menu_icon' => 'dashicons-format-video',
+        'menu_icon' => 'dashicons-calendar',
         'labels' => array(
             'name' => 'Classes',
             'add_new_item' => 'Neue Class',
@@ -85,30 +77,13 @@ function pk_post_types(){
         )
     ) );
 
-    // Post Type Video
-    register_post_type( 'video', array(
-        'public' => true,
-        'has_archive' => true,
-        'show_in_rest' => true,
-        'supports' => array('title', 'editor','excerpt', 'thumbnail'),  
-        'rewrite' => array(
-            'slug' => 'videos'  
-        ),
-        'menu_icon' => 'dashicons-format-video',
-        'labels' => array(
-            'name' => 'Videos',
-            'add_new_item' => 'Neues Video',
-            'edit_item' => 'Videos bearbeiten',
-            'all_items' => 'Alle Videos',
-            'singular_name' => 'Video'
-        )
-    ) );
-
-    // Post Type Referenzen
+    // Post Type referenz
     register_post_type( 'referenz', array(
         'public' => true,
         'has_archive' => true,
         'show_in_rest' => true,
+        'capability_type' => 'referenz', # zur Rechtesteuerung in Members
+        'map_meta_cap' => true,
         'supports' => array('title', 'editor','excerpt', 'thumbnail'),   
         'rewrite' => array(
             'slug' => 'referenzen'  
@@ -123,21 +98,27 @@ function pk_post_types(){
         )
     ) );
 
-    // Post Type News
-    register_post_type( 'news', array(
+    // Post Type clipping
+    register_post_type( 'clipping', array(
         'public' => true,
         'has_archive' => true,
         'show_in_rest' => true,
+        'capability_type' => 'clipping', # zur Rechtesteuerung in Members
+        'map_meta_cap' => true,
         'supports' => array('title', 'editor','excerpt', 'thumbnail'),   
-        'menu_icon' => 'dashicons-media-text',
+        'rewrite' => array(
+            'slug' => 'clippings'  
+        ),
+        'menu_icon' => 'dashicons-media-document',
         'labels' => array(
-            'name' => 'News',
-            'add_new_item' => 'Neue News',
-            'edit_item' => 'News bearbeiten',
-            'all_items' => 'Alle News',
-            'singular_name' => 'News'
+            'name' => 'Clippings',
+            'add_new_item' => 'Neues Clipping',
+            'edit_item' => 'Clipping bearbeiten',
+            'all_items' => 'Alle Clippings',
+            'singular_name' => 'Clipping'
         )
     ) );
+    
 }
 
 add_action( 'init', 'pk_post_types');
@@ -145,27 +126,26 @@ add_action( 'init', 'pk_post_types');
 // ------------------------------------
 
 // Um die Sortierung in Archive zu manipulieren 
-
 function pk_adjust_queries($query){
-    if(!is_admin() AND is_post_type_archive('video') AND $query->is_main_query()){
-        $query->set('orderby', 'title');
-        $query->set('order', 'ASC');
-        $query->set('posts_per_page', -1);
-    }
+    // if(!is_admin() AND is_post_type_archive('video') AND $query->is_main_query()){
+    //     $query->set('orderby', 'title');
+    //     $query->set('order', 'ASC');
+    //     $query->set('posts_per_page', -1);
+    // }
 
-    if(!is_admin() AND is_post_type_archive('veranstaltung') AND $query->is_main_query()){
+    if(!is_admin() AND is_post_type_archive('clipping') AND $query->is_main_query()){
         $today = date('Ymd');
-        $query->set('meta_key', 'veranstaltungsdatum');
+        $query->set('meta_key', 'publication_date');
         $query->set('orderby', 'meta_value_num');
-        $query->set('order', 'ASC');
-        $query->set('meta_query', array(
-            array(
-                'key' => 'veranstaltungsdatum',
-                'compare' => '>=',
-                'value' => $today,
-                'type' => 'numeric'
-            )
-        ));
+        $query->set('order', 'DESC');
+        // $query->set('meta_query', array(
+        //     array(
+        //         'key' => 'publication_date',
+        //         'compare' => '>=',
+        //         'value' => $today,
+        //         'type' => 'numeric'
+        //     )
+        // ));
     }
 }
 
@@ -174,17 +154,16 @@ add_action( 'pre_get_posts', 'pk_adjust_queries' );
 // -----------------------------------------
 
 // Zur Nutzung von Google Maps
+// function pk_map_key($api){
+//     $api['key'] = 'AIzaSyD7J5yFUOtb0wI5ijYcJeS1WujBviE5zQY';
+//     return $api;
+// }
 
-function pk_map_key($api){
-    $api['key'] = 'AIzaSyD7J5yFUOtb0wI5ijYcJeS1WujBviE5zQY';
-    return $api;
-}
-
-add_filter('acf/fields/google_map/api', 'pk_map_key');
+// add_filter('acf/fields/google_map/api', 'pk_map_key');
 
 // -------------------------------------------
-// Redirect subscriber accounts to homepage
 
+// Redirect subscriber accounts to homepage
 function redirectSubsToFrontend(){
     $currentUser = wp_get_current_user();
     if(count($currentUser->roles) == 1 AND $currentUser->roles[0] == 'subscriber'){
@@ -196,7 +175,6 @@ function redirectSubsToFrontend(){
 add_action('admin_init', 'redirectSubsToFrontend');
 
 // hide admin bar for subscribers
-
 function noSubsBar(){
     $currentUser = wp_get_current_user();
     if(count($currentUser->roles) == 1 AND $currentUser->roles[0] == 'subscriber'){
@@ -207,6 +185,7 @@ function noSubsBar(){
 add_action('wp_loaded', 'noSubsBar');
 
 // --------------------------------------------
+
 // Login Window
 
 // Title Link ändern
@@ -217,11 +196,12 @@ function ourHeaderUrl(){
 add_filter('login_headerurl','ourHeaderUrl');
 
 
-// Titel ändern
+// Titel/Logo ändern
 function ourLoginTitle(){
-    // return get_bloginfo('name');
-    return 'Peter seine Website';
+    return get_bloginfo('name');
+    // return 'Peter seine Website';
 }
 
 add_filter('login_headertext','ourLoginTitle');
+
 
